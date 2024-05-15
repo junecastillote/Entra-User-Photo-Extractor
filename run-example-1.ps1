ec4823dc2932d1bea22d43d1c533c5b49487cf8c
June Castillote <tito.castillote-jr@dxc.com>
2024-05-15 09:19:57 +0800

    Renamed

--- run-example-1.ps1
+++ run-example-1.ps1
@@ -1,8 +1,8 @@
 Connect-MgGraph -TenantId "tenant.onmicrosoft.com"
 
 ## Export photo of all users (if available)
-.\Export-UserPhoto.ps1 -OutFolder "PHOTO DUMP FOLDER PATH"
+.\Export-EntraIdUserPhoto.ps1 -OutFolder "PHOTO DUMP FOLDER PATH"
 
 ## Export photo of specific users
 $userId = @("user_a@contoso.com", "user_b@contoso.com", "user_c@contoso.com")
-.\Export-UserPhoto.ps1 -UserId $userId -OutFolder "PHOTO DUMP FOLDER PATH"
+.\Export-EntraIdUserPhoto.ps1 -UserId $userId -OutFolder "PHOTO DUMP FOLDER PATH"
